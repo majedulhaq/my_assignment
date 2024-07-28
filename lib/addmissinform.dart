f58c9612ab787053fc1ac53303ab9764e88f35ef
majedul_haq <majedulhaq550@gmail.com>
2024-07-28 22:40:15 +0600

    form

---
+++ lib/addmissinform.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class Addform extends StatefulWidget {
+  const Addform({super.key});
+
+  @override
+  State<Addform> createState() => _AddformState();
+}
+
+class _AddformState extends State<Addform> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(12),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  "আপনার নাম লিখুন",
+                  style: TextStyle(fontSize: 23, color: Color(0xff89967A)),
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xff89967A))),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  "জামাতের নাম লিখুন",
+                  style: TextStyle(fontSize: 23, color: Color(0xff89967A)),
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xff89967A))),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  "মোবাইল নাম্বার লিখুন",
+                  style: TextStyle(fontSize: 23, color: Color(0xff89967A)),
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xff89967A))),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  "অভিভাবকের নাম লিখুন",
+                  style: TextStyle(fontSize: 23, color: Color(0xff89967A)),
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xff89967A))),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,70 +1,18 @@
-// // Current SDK version: 3.22.1
-// // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+import 'package:flutter/material.dart';
+import 'package:my_assignment/addmissinform.dart';
 
-// import 'package:flutter/material.dart';
-// // import 'package:flutter/src/widgets/app.dart';
+void main(List<String> args) {
+  runApp(MyApp());
+}
 
-// void main() {
-//   runApp(MyApp());
-// }
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-// class MyApp extends StatelessWidget {
-//   MyApp({super.key});
-
-//   List jamat = [
-//     'তাকমীল ফিল হাদীস',
-//     'ফযিলত ২য় বর্ষ',
-//     'ফযিলত ১ম বর্ষ',
-//   ];
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       home: SafeArea(
-//         child: Scaffold(
-//           appBar: AppBar(
-//             backgroundColor: Color(0xAAE0EDDC),
-//             title: Text(
-//               'ATTENDENCE',
-//               style: TextStyle(color: Color(0xAA6A7381)),
-//             ),
-//             centerTitle: true,
-//           ),
-//           body: Column(
-//             children: [
-//               buildtile(jamat[0], Icons.done, 0),
-//               buildtile(jamat[1], Icons.dangerous_outlined, 2),
-//               buildtile(jamat[2], Icons.add, 3),
-
-//               // ListTile(
-//               //   leading: CircleAvatar(),
-//               //   title: Text('তাকমীল ফিল হাদীস', style: TextStyle(fontSize: 22)),
-//               //   trailing: Icon(Icons.done, size: 30),
-//               // ),
-//               // ListTile(
-//               //   leading: CircleAvatar(),
-//               //   title: Text('ফযিলত ২য় বর্ষ', style: TextStyle(fontSize: 22)),
-//               //   trailing: Icon(Icons.done, size: 30),
-//               // ),
-//               // ListTile(
-//               //   leading: CircleAvatar(),
-//               //   title: Text('ফযিলত ১ম বর্ষ', style: TextStyle(fontSize: 22)),
-//               //   trailing: Icon(Icons.done, size: 30, color: Colors.green),
-//               // )
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-
-//   ListTile buildtile(String name, IconData icon, int num) {
-//     return ListTile(
-//         leading: CircleAvatar(
-//           child: Text(num.toString()),
-//         ),
-//         title: Text(name, style: TextStyle(fontSize: 22)),
-//         trailing: Icon(icon, size: 30));
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Addform(),
+    );
+  }
+}
