aa327e3355235eb6e90083ba95d8ca15beef20bb
majedul_haq <majedulhaq550@gmail.com>
2024-07-28 22:16:53 +0600

    list of student

---
+++ lib/homepage.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<Todo> jamat = []; // Define your Todo list
+  TextEditingController taskc = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Builder(builder: (context) {
+          return Scaffold(
+            floatingActionButton: FloatingActionButton(
+              onPressed: () {
+                showModalBottomSheet(
+                    context: context,
+                    builder: (context) {
+                      return Container(
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
+                            children: [
+                              TextField(
+                                controller: taskc,
+                                decoration: InputDecoration(
+                                    border: OutlineInputBorder()),
+                              ),
+                              ElevatedButton(
+                                  onPressed: () {
+                                    jamat.add(Todo(taskc.text.trim(), false));
+                                    setState(() {});
+                                    taskc.clear();
+                                  },
+                                  child: Text('Save'))
+                            ],
+                          ),
+                        ),
+                      );
+                    });
+              },
+              child: const Icon(Icons.add),
+            ),
+            appBar: AppBar(
+              backgroundColor: const Color(0xAAE0EDDC),
+              title: const Text(
+                'Preাাাsent',
+                style: TextStyle(color: Color(0xAA6A7381)),
+              ),
+              centerTitle: true,
+            ),
+            body: ListView.builder(
+              itemCount: jamat.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                    leading: CircleAvatar(
+                      backgroundColor: Colors.grey.shade300,
+                      
+                      child: Text((index + 1).toString()),
+                    ),
+                    title: Text(jamat[index].task),
+                    trailing: IconButton(
+                      onPressed: () {
+                        jamat[index].isdone = !jamat[index].isdone;
+                        setState(() {});
+                      },
+                      icon: jamat[index].isdone == true
+                          ? Icon(Icons.done, color: Colors.green)
+                          : Icon(Icons.close, color: Colors.red),
+                    ));
+              },
+            ),
+          );
+        }),
+      ),
+    );
+  }
+}
+
+class Todo {
+  String task;
+  bool isdone;
+  Todo(this.task, this.isdone);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,77 +1,70 @@
-// Current SDK version: 3.22.1
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+// // Current SDK version: 3.22.1
+// // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
+// // import 'package:flutter/src/widgets/app.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+// void main() {
+//   runApp(MyApp());
+// }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+// class MyApp extends StatelessWidget {
+//   MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Home(),
-      title: 'My App',
-    );
-  }
-}
+//   List jamat = [
+//     'তাকমীল ফিল হাদীস',
+//     'ফযিলত ২য় বর্ষ',
+//     'ফযিলত ১ম বর্ষ',
+//   ];
 
-class Home extends StatefulWidget {
-  const Home({super.key});
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: SafeArea(
+//         child: Scaffold(
+//           appBar: AppBar(
+//             backgroundColor: Color(0xAAE0EDDC),
+//             title: Text(
+//               'ATTENDENCE',
+//               style: TextStyle(color: Color(0xAA6A7381)),
+//             ),
+//             centerTitle: true,
+//           ),
+//           body: Column(
+//             children: [
+//               buildtile(jamat[0], Icons.done, 0),
+//               buildtile(jamat[1], Icons.dangerous_outlined, 2),
+//               buildtile(jamat[2], Icons.add, 3),
 
-  @override
-  State<Home> createState() => _HomeState();
-}
+//               // ListTile(
+//               //   leading: CircleAvatar(),
+//               //   title: Text('তাকমীল ফিল হাদীস', style: TextStyle(fontSize: 22)),
+//               //   trailing: Icon(Icons.done, size: 30),
+//               // ),
+//               // ListTile(
+//               //   leading: CircleAvatar(),
+//               //   title: Text('ফযিলত ২য় বর্ষ', style: TextStyle(fontSize: 22)),
+//               //   trailing: Icon(Icons.done, size: 30),
+//               // ),
+//               // ListTile(
+//               //   leading: CircleAvatar(),
+//               //   title: Text('ফযিলত ১ম বর্ষ', style: TextStyle(fontSize: 22)),
+//               //   trailing: Icon(Icons.done, size: 30, color: Colors.green),
+//               // )
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
 
-class _HomeState extends State<Home> {
-  List<String> friendList = [
-    'Fahad',
-    'Iram',
-    'Touhid',
-    'Sakib',
-    'Niloy',
-    'Rahat',
-    'Khaled',
-    'Sakib',
-    'Niloy',
-    'Rahat',
-    'Khaled'
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          title: const Text('my app'),
-          centerTitle: true,
-          backgroundColor: Colors.blue,
-        ),
-        body: ListView.separated(
-          itemCount: friendList.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              
-              onTap: () {},
-              title: Text(friendList[index]),
-              subtitle: Text('this is subtitle'),
-              leading: CircleAvatar(
-                backgroundColor: Colors.grey.withOpacity(.3),
-                child: Text('${index + 1}'),
-              ),
-              trailing: Icon(Icons.arrow_forward_ios_outlined),
-            );
-          },
-          separatorBuilder: (context, index) {
-            return Divider(
-              thickness: 9,
-              color: Colors.red.withOpacity(.2),
-            );
-          },
-        ));
-  }
-}
+//   ListTile buildtile(String name, IconData icon, int num) {
+//     return ListTile(
+//         leading: CircleAvatar(
+//           child: Text(num.toString()),
+//         ),
+//         title: Text(name, style: TextStyle(fontSize: 22)),
+//         trailing: Icon(icon, size: 30));
+//   }
+// }
