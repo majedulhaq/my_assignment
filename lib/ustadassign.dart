a4e9cc0f61e55e9e3181e034d2025b7396e19ab4
majedul_haq <majedulhaq550@gmail.com>
2024-08-28 16:12:56 +0600

    last

--- lib/ustadassign.dart
+++ lib/ustadassign.dart
@@ -43,93 +43,95 @@ class Responsivedesign extends StatelessWidget {
 class LaptopOrLargeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(50.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text('Humming\nBird',
-                  textAlign: TextAlign.left,
-                  style: TextStyle(
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  )),
-              Row(
-                children: [
-                  TextButton(
-                    onPressed: () {},
-                    child: Text('Episodes',
-                        style: TextStyle(
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                        )),
-                  ),
-                  TextButton(
-                    onPressed: () {},
-                    child: Text('About',
-                        style: TextStyle(
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                        )),
-                  ),
-                ],
-              ),
-            ],
-          ),
-          SizedBox(height: 150),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: const [
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(50.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('Humming\nBird',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    )),
+                Row(
                   children: [
-                    Text(
-                      'FLUTTER WEB.\nTHE BASICS',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 60,
-                        fontWeight: FontWeight.bold,
-                      ),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text('Episodes',
+                          style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          )),
                     ),
-                    SizedBox(height: 20),
-                    Text(
-                        "In this course we will go over the basics of using Flutter Web for development. Topics will includeResponsive Layout, Deploying, Font Changes, Hoverfunctionality, Modals and more.",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: 29,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                        )),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text('About',
+                          style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          )),
+                    ),
                   ],
                 ),
-              ),
-              SizedBox(width: 100),
-              ElevatedButton(
-                  onPressed: null,
-                  child: Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Text(
-                      "Join Course",
-                      style: TextStyle(color: Colors.white, fontSize: 30),
-                    ),
+              ],
+            ),
+            SizedBox(height: 150),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: const [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        'FLUTTER WEB.\nTHE BASICS',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 60,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Text(
+                          "In this course we will go over the basics of using Flutter Web for development. Topics will includeResponsive Layout, Deploying, Font Changes, Hoverfunctionality, Modals and more.",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 29,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          )),
+                    ],
                   ),
-                  style: ButtonStyle(
-                      backgroundColor:
-                          WidgetStatePropertyAll(Color(0xff00AF7C)),
-                      shape: WidgetStatePropertyAll(RoundedRectangleBorder(
-                          borderRadius:
-                              BorderRadius.all(Radius.circular(5)))))),
-              SizedBox(width: 100),
-            ],
-          ),
-        ],
+                ),
+                SizedBox(width: 100),
+                ElevatedButton(
+                    onPressed: null,
+                    child: Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text(
+                        "Join Course",
+                        style: TextStyle(color: Colors.white, fontSize: 30),
+                      ),
+                    ),
+                    style: ButtonStyle(
+                        backgroundColor:
+                            WidgetStatePropertyAll(Color(0xff00AF7C)),
+                        shape: WidgetStatePropertyAll(RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(5)))))),
+                SizedBox(width: 100),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -140,86 +142,93 @@ class TabletView extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Column(
-          children: const [
-            Row(
-              children: [
-                SizedBox(width: 20),
-                Text('Humming\nBird',
-                    textAlign: TextAlign.left,
-                    style: TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                    )),
-                Expanded(child: SizedBox(width: 20)),
-                Text('Episodes',
-                    style: TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                    )),
-                SizedBox(width: 50),
-                Text('About',
-                    style: TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                    )),
-                SizedBox(width: 20)
-              ],
-            ),
-            SizedBox(
-              height: 150,
-            ),
-            Center(
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("FLUTTER WEB.\nTHE BASICS",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: 40,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                        )),
-                    SizedBox(
-                      height: 45,
+        body: SingleChildScrollView(
+          child: Column(
+            children: const [
+              Row(
+                children: [
+                  SizedBox(width: 20),
+                  Text('Humming\nBird',
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      )),
+                  Expanded(child: SizedBox(width: 20)),
+                        TextButton(
+                      onPressed: null,
+                      child: Text('Episodes',
+                          style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          )),
                     ),
-                    Text(
-                        "In this course we will go over the basics of using Flutter Web for development. Topics will includeResponsive Layout, Deploying, Font Changes, Hoverfunctionality, Modals and more.",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: 29,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                        )),
-                    SizedBox(
-                      height: 120,
+                    TextButton(
+                      onPressed: null,
+                      child: Text('About',
+                          style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          )),
                     ),
-                    ElevatedButton(
-                        onPressed: null,
-                        child: Padding(
-                          padding: EdgeInsets.all(8.0),
-                          child: Text(
-                            "Join Course",
-                            style: TextStyle(color: Colors.white, fontSize: 30),
+                  SizedBox(width: 20)
+                ],
+              ),
+              SizedBox(
+                height: 150,
+              ),
+              Center(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("FLUTTER WEB.\nTHE BASICS",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 40,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          )),
+                      SizedBox(
+                        height: 45,
+                      ),
+                      Text(
+                          "In this course we will go over the basics of using Flutter Web for development. Topics will includeResponsive Layout, Deploying, Font Changes, Hoverfunctionality, Modals and more.",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 29,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          )),
+                      SizedBox(
+                        height: 120,
+                      ),
+                      ElevatedButton(
+                          onPressed: null,
+                          child: Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Text(
+                              "Join Course",
+                              style: TextStyle(color: Colors.white, fontSize: 30),
+                            ),
                           ),
-                        ),
-                        style: ButtonStyle(
-                            backgroundColor:
-                                WidgetStatePropertyAll(Color(0xff00AF7C)),
-                            shape: WidgetStatePropertyAll(
-                                RoundedRectangleBorder(
-                                    borderRadius:
-                                        BorderRadius.all(Radius.circular(5))))))
-                  ],
+                          style: ButtonStyle(
+                              backgroundColor:
+                                  WidgetStatePropertyAll(Color(0xff00AF7C)),
+                              shape: WidgetStatePropertyAll(
+                                  RoundedRectangleBorder(
+                                      borderRadius:
+                                          BorderRadius.all(Radius.circular(5))))))
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
